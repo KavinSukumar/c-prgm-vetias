235edd1ccffe971511a73c49dfc7111d704e2429
KavinSukumar <kavins23sit@vetias.ac.in>
2025-05-12 14:28:42 +0530

    Add files via upload

---
+++ placement code/main (1).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("enter the two value:");
+    scanf("%d",&a);
+    
+    printf("enter the second value:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("sum:%d",sum);
+    return 0;
+}
